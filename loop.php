c0fed166ac3217458b9ed3f00418078d9565c86f
Adam Hope <adam@gothikon.com>
2010-08-01 19:49:48 +1000

    removing accidental commit

--- loop.php
+++ loop.php
@@ -18,13 +18,6 @@ if (have_posts()) : while (have_posts()) : the_post();
 
 <?php if ( in_category( _x('photos', 'photo category slug', 'reflection') ) ) : ?>
 
-  <?php if ( $wp_query->max_num_pages > 1 ) : ?>
-  	<div id="nav-above" class="navigation">
-  		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
-  		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
-  	</div><!-- #nav-above -->
-  <?php endif; ?>
-
   <?
   // Grab next/previous post IDs if they exist and are in same category. Odd syntax is hack for PHP4.
   $next_post      = is_object($npobj = get_next_post(true))     ? $npobj->ID : 0;
