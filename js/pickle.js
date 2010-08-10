e94b4a3f32c099c10f3538cac8b733b82fbc8db2
Adam Hope <adam@gothikon.com>
2010-08-10 20:47:12 +1000

    exif data now updated

--- js/pickle.js
+++ js/pickle.js
@@ -4,8 +4,8 @@ var Pickle = (function(user_opts){
   DEFAULTS = {
     context: $('#topcontent'),
     mainImage: $('#mainimage'),
-    nextLink: $('#overNextLink'),
-    previousLink: $('#overPrevLink')
+    nextLink: $('#overNextLink'),     // nextPostLink
+    previousLink: $('#overPrevLink') // prevPostLink
   },
   
   preloadedImg,
@@ -17,6 +17,10 @@ var Pickle = (function(user_opts){
         e.preventDefault();        
       });
     });
+    $('#exif').click(function(e){
+      $('#panel_exif').toggle();
+      e.preventDefault();
+    });
   },
   
   getNewContent = function (el) {
@@ -24,7 +28,6 @@ var Pickle = (function(user_opts){
   		postID = cfg.prevPostID,
 		  params = '?id=' + postID;
       $.getJSON(url + params, update);
-      // set loading image
   },
   
   loadComplete = function() {
@@ -32,18 +35,21 @@ var Pickle = (function(user_opts){
     $('#topcontent').css({width: preloadedImg.width});
 		$(mainImage)[0].width  = preloadedImg.width;
 		$(mainImage)[0].height = preloadedImg.height;
-    // $(mainImage).attr('src', preloadedImg.src);
+    $(mainImage).attr('src', preloadedImg.src);
  	 };
   
   update = function (data) {
+    console.debug(data);
     // fade out image
     // resize image area
     // fade in new image
     preloadedImg = new Image();
     preloadedImg.onload = loadComplete;
     preloadedImg.src = data.image_uri;
-    $('#mainimage').css({'visibility':'hidden'});
+    // $('#mainimage').css({'visibility':'hidden'});
 		$('#imageholder').css('background-image', cfg.templateDir + '/images/loading.gif');
+    // update exif
+    $('#panel_exif').html(data.exif);
   };
   
   return {
