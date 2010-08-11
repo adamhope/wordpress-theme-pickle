be2e4aa845d53beab950429adf3e9a20a600c228
Adam Hope <adam@gothikon.com>
2010-08-11 21:04:24 +1000

    beautifying code

--- js/pickle.js
+++ js/pickle.js
@@ -1,101 +1,95 @@
-(function($) {
-  $.fn.toggleFade = function(settings)
-  {
-    settings = jQuery.extend(
-      {
-        speedIn: "normal",
-        speedOut: "normal"
-      }, settings
-    );
-    return this.each(function()
-    {
+(function ($) {
+  $.fn.toggleFade = function (settings) {
+    settings = jQuery.extend({
+      speedIn: "normal",
+      speedOut: "normal"
+    }, settings);
+    return this.each(function () {
       var isHidden = jQuery(this).is(":hidden");
-      jQuery(this)[ isHidden ? "fadeIn" : "fadeOut" ]( isHidden ? settings.speedIn : settings.speedOut);
+      jQuery(this)[isHidden ? "fadeIn" : "fadeOut"](isHidden ? settings.speedIn : settings.speedOut);
     });
   };
 })(jQuery);
 
-var Pickle = (function(user_opts){
+var Pickle = (function (user_opts) {
 
-  var cfg,
-  preloadedImg,
-  nextPostID,
-  prevPostID,
-  ajaxSource,
-  DEFAULTS = {
+  var cfg, preloadedImg, nextPostID, prevPostID, ajaxSource, DEFAULTS = {
     context: '#topcontent',
     mainImage: '#mainimage'
   },
 
   setupNavigation = function () {
-    $('.previous, .next').each(function(i, el) {
-      $(el).click(function(e){
+    $('.previous, .next').each(function (i, el) {
+      $(el).click(function (e) {
         getNewContent(el);
         e.preventDefault();
       });
     });
-    $('#exif').click(function(e){
+    $('#exif').click(function (e) {
       $('#panelExif').toggleFade();
       e.preventDefault();
     });
-    $('#info').click(function(e){
+    $('#info').click(function (e) {
       $('#panelInfo').toggleFade();
       e.preventDefault();
     });
   },
 
-  loadComplete = function() {
-    $('#topcontent').css({width: preloadedImg.width});
-    $('#mainimage').attr({
-      'width':  preloadedImg.width,
-      'height': preloadedImg.height,
-      'src':    preloadedImg.src
+  loadComplete = function () {
+    $('#topcontent').css({
+      width: preloadedImg.width
     });
-   },
+    $('#mainimage').attr({
+      'width': preloadedImg.width,
+      'height': preloadedImg.height,
+      'src': preloadedImg.src
+    });
+  },
 
   refresh = function (data) {
     // fade out image
-    // resize image area
     // fade in new image
     preloadedImg = new Image();
     preloadedImg.onload = loadComplete;
     preloadedImg.src = data.image_uri;
 
     // $('#mainimage').css({'visibility': 'hidden'});
-
     nextPostID = data.next_post;
     prevPostID = data.prev_post;
 
     $('#nextPostLink').html(data.next_post == 0 ? '' : '&raquo;');
     $('#prevPostLink').html(data.prev_post == 0 ? '' : '&laquo;');
 
-    $('#overNextLink').css({'display': data.next_post == 0 ? 'none' : 'block'});
-    $('#overPrevLink').css({'display': data.prev_post == 0 ? 'none' : 'block'});
+    $('#overNextLink').css({
+      'display': data.next_post == 0 ? 'none' : 'block'
+    });
+    $('#overPrevLink').css({
+      'display': data.prev_post == 0 ? 'none' : 'block'
+    });
 
     // this.nextPostLink.href = $('#overNextLink').href = data.next_post_perm;
     // this.prevPostLink.href = $('#overPrevLink').href = data.prev_post_perm;
-
     $('#panelExif').html(data.exif);
     $('#comment').html(data.comment_count + " comment" + (data.comment_count != 1 ? "s" : ""));
-    $('#comment').attr({'href': data.permalink + '#comments'});
+    $('#comment').attr({
+      'href': data.permalink + '#comments'
+    });
     $('#textTitle').html('<a href="' + data.permalink + '">' + data.post_title + '</a><span id="inlinedate">' + data.post_date + '</span>');
     $('#panelInfo').html(data.post_content);
 
   },
 
   getNewContent = function (el) {
-    var postID,
-        params,
-        direction = $(el).attr('class');
-      if (direction == 'previous') {
-        postID = prevPostID;
-      } else if (direction == 'next') {
-        postID = nextPostID;
-      } else {
-        return false;
-      }
-      params = '?id=' + postID;
-      $.getJSON(ajaxSource + params, refresh);
+    var postID, params, direction = $(el).attr('class');
+    if (direction == 'previous') {
+      postID = prevPostID;
+    } else if (direction == 'next') {
+      postID = nextPostID;
+    } else {
+      return false;
+    }
+    params = '?id=' + postID;
+    $.getJSON(ajaxSource + params, refresh);
   };
 
   return {
