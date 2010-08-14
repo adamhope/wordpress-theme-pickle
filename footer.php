c7393f92f4ee77bfe871909920dbe805fed8b4cb
Adam Hope <adam@gothikon.com>
2010-08-14 10:09:15 +1000

    minimsed pickle.js, fixed parsing error in document.write

--- footer.php
+++ footer.php
@@ -10,11 +10,12 @@
         </a>
 
         <?php do_action( 'reflection_credits' ); ?>
+
         <a href="<?php echo esc_url( __('http://wordpress.org/', 'pickle') ); ?>"
         title="<?php esc_attr_e('Semantic Personal Publishing Platform', 'pickle'); ?>" rel="generator">
           <?php printf( __('Proudly powered by %s.', 'pickle'), 'WordPress' ); ?>
         </a>
-      </small><!-- #site-generator -->
+      </small>
 
     </div><!-- #colophon -->
   </footer>
@@ -23,10 +24,10 @@
 
   <!-- Grab Google CDN's jQuery. fall back to local if necessary -->
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-  <script>!window.jQuery && document.write('<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/jquery-1.4.2.min.js"></script>')</script>
+  <script>!window.jQuery && document.write('<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/jquery-1.4.2.min.js"></sc'+'ript>')</script>
   <script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/tooltip.js"></script>
 
-  <script src="<?php bloginfo('template_directory');?>/js/pickle.js" type="text/javascript" charset="utf-8"></script>
+  <script src="<?php bloginfo('template_directory');?>/js/pickle.min.js" type="text/javascript" charset="utf-8"></script>
 
   <script type="text/javascript">
     browseOpts.templateDir = '<?php bloginfo('template_directory');?>';
