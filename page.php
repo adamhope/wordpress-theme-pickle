45f0f0a221c04cb3a874a9e6cffd22451dbfd366
Adam Hope <adam@gothikon.com>
2010-08-14 10:09:46 +1000

    page.php now uses more consistent HTML structure

--- page.php
+++ page.php
@@ -10,7 +10,7 @@ get_header();
 if (have_posts()) : while (have_posts()) : the_post();
 ?>
 
-<div id="pagecontent">
+<div id="main">
   <h2><?php the_title();?></h2>
   <?php the_content('Read more...');?>
 </div>
