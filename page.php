cdf61edff4ddeae3b61f5b1469e25cc1a111d7ce
Adam Hope <adam@gothikon.com>
2010-08-19 15:23:21 +1000

    now using article tag

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 /**
  * Standard page template.
  *
- * @package Reflection
+ * @package Pickle
  */
 
 get_header();
@@ -11,8 +11,10 @@ if (have_posts()) : while (have_posts()) : the_post();
 ?>
 
 <div id="main">
-  <h2><?php the_title();?></h2>
-  <?php the_content('Read more...');?>
+  <article>
+    <h2><?php the_title();?></h2>
+    <?php the_content('Read more...');?>
+  </article>
 </div>
 
 <?php
