19650accef6230e1860688333e4c50a3a4e8088b
Adam Hope <adam@gothikon.com>
2010-08-19 18:14:20 +0100

    now using article tag for posts

--- loop-archive.php
+++ loop-archive.php
@@ -29,13 +29,13 @@
 
 <?php /* If there are no posts to display, such as an empty archive page */ ?>
 <?php if ( ! have_posts() ) : ?>
-	<div id="post-0" class="post error404 not-found">
+	<article id="post-0" class="post error404 not-found">
 		<h1 class="entry-title"><?php _e( 'Not Found', 'twentyten' ); ?></h1>
 		<div class="entry-content">
 			<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyten' ); ?></p>
 			<?php get_search_form(); ?>
-		</div><!-- .entry-content -->
-	</div><!-- #post-0 -->
+		</div>
+	</article>
 <?php endif; ?>
 
 <?php
@@ -63,17 +63,17 @@
 
 			<div class="entry-meta">
 				<?php pickle_posted_on(); ?>
-			</div><!-- .entry-meta -->
+			</div>
 
 	<?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
 			<div class="entry-summary">
 				<?php the_excerpt(); ?>
-			</div><!-- .entry-summary -->
+			</div>
 	<?php else : ?>
 			<div class="entry-content">
 				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?>
 				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
-			</div><!-- .entry-content -->
+			</div>
 	<?php endif; ?>
 
 			<div class="entry-utility">
