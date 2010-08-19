810ea253b3d537c6dab977d4996f935a33b15575
Adam Hope <adam@gothikon.com>
2010-08-19 18:13:45 +0100

    Fixed identation

--- comments.php
+++ comments.php
@@ -36,7 +36,6 @@
 	<?php $i = 0; foreach ($comments as $comment) : $i++; ?>
 
 		<article class="comment" <?php echo $oddcomment; ?>id="comment-<?php comment_ID() ?>">
-      <!-- <div class="commentcount"><?=$i?></div> -->
 			<h3 class="comment-metadata">
 			  <?php comment_author_link() ?>
 			  <time><?php comment_date('jS F, Y') ?> at <?php comment_time() ?></time>
@@ -81,31 +80,31 @@
 
 <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
 
-<?php if ( $user_ID ) : ?>
+  <?php if ( $user_ID ) : ?>
 
-<p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="Log out of this account">Logout &raquo;</a></p>
+  <p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="Log out of this account">Logout &raquo;</a></p>
 
-<?php else : ?>
+  <?php else : ?>
 
-<p><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" />
-<label for="author"><small>Name <?php if ($req) echo "(required)"; ?></small></label></p>
+  <p><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" />
+  <label for="author"><small>Name <?php if ($req) echo "(required)"; ?></small></label></p>
 
-<p><input type="email" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" />
-<label for="email"><small>Mail (will not be published) <?php if ($req) echo "(required)"; ?></small></label></p>
+  <p><input type="email" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" />
+  <label for="email"><small>Mail (will not be published) <?php if ($req) echo "(required)"; ?></small></label></p>
 
-<p><input type="url" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />
-<label for="url"><small>Website</small></label></p>
+  <p><input type="url" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />
+  <label for="url"><small>Website</small></label></p>
 
-<?php endif; ?>
+  <?php endif; ?>
 
-<!--<p><small><strong>XHTML:</strong> You can use these tags: <code><?php echo allowed_tags(); ?></code></small></p>-->
+  <!--<p><small><strong>XHTML:</strong> You can use these tags: <code><?php echo allowed_tags(); ?></code></small></p>-->
 
-<p><textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>
+  <p><textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>
 
-<p><input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
-<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
-</p>
-<?php do_action('comment_form', $post->ID); ?>
+  <p><input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
+  <input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
+  </p>
+  <?php do_action('comment_form', $post->ID); ?>
 
 </form>
 
