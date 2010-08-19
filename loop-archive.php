b635d89414608c4fec4abe7e4dc3f3255118653b
Adam Hope <adam@gothikon.com>
2010-08-19 15:23:39 +1000

    now using article tag

--- loop-archive.php
+++ loop-archive.php
@@ -58,6 +58,7 @@
 <?php /* How to display posts in the Gallery category. */ ?>
 
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		  <article>
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
 			<div class="entry-meta">
@@ -94,6 +95,7 @@
 				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
 				<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
 			</div><!-- .entry-utility -->
+			</article>
 		</div><!-- #post-## -->
 
 		<?php comments_template( '', true ); ?>
