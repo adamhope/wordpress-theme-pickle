ec33a08837621bbbaae092415ff556c0a51d7571
Adam Hope <adam@gothikon.com>
2011-02-05 13:17:05 +1100

    improved custom image sizes for featured images and photo category thumbs

--- functions.php
+++ functions.php
@@ -498,10 +498,13 @@ if ( function_exists( 'add_theme_support' ) ) {
   
   set_post_thumbnail_size( 150, 150, true ); // default Post Thumbnail dimensions (cropped)
 
-  // additional image sizes
-  add_image_size( 'mosaic-thumb', 280, 280 ); // last param is crop
+  // Custom image sizes
+  // NOTE: seem need to re-upload post image for change to take effect?
+  // NOTE: last param is crop
+  add_image_size( 'mosaic-thumb', 280, 280, true );
   add_image_size( 'single', 480, 999 );
   add_image_size( 'slideshow', 480, 400 );
+  add_image_size( 'featuredImage', 999, 480 );
   
 }
 
