e73bcc6a7567a3540153c7da8a00ccefd84734b0
Adam Hope <adam@gothikon.com>
2011-02-13 21:29:35 +1100

    single posts displaying correctly again

--- single.php
+++ single.php
@@ -5,30 +5,9 @@ TODO
 - make cropping configurable
 -->
 
-<?php
-  $photo_category_id = get_opt_or_default('photo_category_id');
-  $n_slices          = get_opt_or_default('slideshow_length');
-  $posts_on_homepage = get_opt_or_default('posts_on_homepage');
-?>
+<?php get_header(); ?>
 
 <div id="main">
-
-<?php if (is_home()) : ?>
-  <section id="featured-content">
-    <div class="slideshow">
-      <?php query_posts('cat=' . $photo_category_id . '&posts_per_page=' . $n_slices); if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-      <?php if(has_post_thumbnail()) : ?>
-        <div class="slide">
-          <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-            <?php the_post_thumbnail('slideshow-slide'); ?>
-          </a>
-        </div>
-      <?php endif ?>
-      <?php endwhile; endif;?>
-      </div>
-  </section>
-  <?php wp_reset_query();?>
-<?php endif; ?>
 
 <section id="content">
 
@@ -43,8 +22,8 @@ TODO
   	</div>
   <?php endif; ?>
 
-<?php query_posts($query_string . '&cat=-' . photo_category_id); ?>
-<?php if ( have_posts() && $posts_on_homepage == 1) : while ( have_posts() ) : the_post(); ?>
+<?php query_posts($query_string); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
   <article class="post">
 
@@ -76,3 +55,5 @@ TODO
 </section>
 
 </div>
+
+<?php get_footer(); ?>
