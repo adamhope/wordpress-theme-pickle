2488b3d1ebd05d9cc660f5c862bb906f62bce512
Adam Hope <adam@gothikon.com>
2011-02-13 21:18:30 +1100

    fixed bug where causing single posts not to display

--- loop-archive.php
+++ loop-archive.php
@@ -38,21 +38,6 @@
 	</article>
 <?php endif; ?>
 
-<?php
-	/* Start the Loop.
-	 *
-	 * In Twenty Ten we use the same loop in multiple contexts.
-	 * It is broken into three main parts: when we're displaying
-	 * posts that are in the gallery category, when we're displaying
-	 * posts in the asides category, and finally all other posts.
-	 *
-	 * Additionally, we sometimes check for whether we are on an
-	 * archive page, a search page, etc., allowing for small differences
-	 * in the loop on each template without actually duplicating
-	 * the rest of the loop that is shared.
-	 *
-	 * Without further ado, the loop:
-	 */ ?>
 <section class="posts">
 <?php while ( have_posts() ) : the_post(); ?>
 
--- loop-index.php
+++ loop-index.php
@@ -9,12 +9,12 @@ TODO
 <div id="main">
 
   <?php
-    if (is_home()) :
     $photo_category_id = get_opt_or_default('photo_category_id');
     $n_slices          = get_opt_or_default('slideshow_length');
     $posts_on_homepage  =get_opt_or_default('posts_on_homepage');
   ?>
 
+<?php if (is_home()) : ?>
   <section id="featured-content">
     <div class="slideshow">
       <?php query_posts('cat=' . $photo_category_id . '&posts_per_page=' . $n_slices); if( have_posts() ) : while( have_posts() ) : the_post(); ?>
@@ -26,10 +26,10 @@ TODO
         </div>
       <?php endif ?>
       <?php endwhile; endif;?>
-      <?php wp_reset_query();?>
       </div>
-    <?php endif; ?>
   </section>
+  <?php wp_reset_query();?>
+<?php endif; ?>
 
 <section id="content">
 
