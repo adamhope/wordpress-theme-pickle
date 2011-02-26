bb3962cacca6669325d419a00a5033a49f234c2d
Adam Hope <adam@gothikon.com>
2011-02-26 15:38:18 +1100

    simplified title for use with Yoast WordPress SEO

--- header.php
+++ header.php
@@ -13,34 +13,15 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>" />
   <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
-  <title><?php
-  	/*
-  	 * Print the <title> tag based on what is being viewed.
-  	 */
-  	global $page, $paged;
-  	wp_title( '|', true, 'right' );
-  	// Add the blog name.
-  	bloginfo( 'name' );
-  	// Add the blog description for the home/front page.
-  	$site_description = get_bloginfo( 'description', 'display' );
-  	if ( $site_description && ( is_home() || is_front_page() ) )
-  		echo " | $site_description";
-  	// Add a page number if necessary:
-  	if ( $paged >= 2 || $page >= 2 )
-  		echo ' | ' . sprintf( __( 'Page %s', 'pickle' ), max( $paged, $page ) );
-	?></title>
-
+  <title><?php wp_title(''); ?></title>
   <link rel="profile" href="http://gmpg.org/xfn/11" />
   <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/reset.css" />
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/3rdparty/orbit/orbit.css">
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
   <?php wp_head(); ?>
-
   <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
   <script src="<?php bloginfo('template_directory');?>/js/modernizr-1.5.min.js"></script>
-
 </head>
 <!--[if lt IE 7 ]> <body class="ie6"> <![endif]-->
 <!--[if IE 7 ]>    <body class="ie7"> <![endif]-->
